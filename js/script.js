4f5594e727b280c501403b68b7d9ba35f3cff28c
saff-it <saff_it@ymail.com>
2022-07-05 16:32:07 +0200

    exercise completed

--- js/script.js
+++ js/script.js
@@ -1,33 +1,3 @@
-// const slides = [
-//     {
-//         image: 'img/01.jpg',
-//         title: 'Svezia',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-//     },
-//     {
-//         image: 'img/02.jpg',
-//         title: 'Svizzera',
-//         text: 'Lorem ipsum.',
-//     },
-//     {
-//         image: 'img/03.jpg',
-//         title: 'Gran Bretagna',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-//     },
-//     {
-//         image: 'img/04.jpg',
-//         title: 'Germania',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-//     },
-//     {
-//         image: 'img/05.jpg',
-//         title: 'Paradise',
-//         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-//     }
-// ];
-
-// console.log(slides);
-
 
 const app = new Vue (
     {
@@ -36,7 +6,7 @@ const app = new Vue (
         data: {  
 
             index : 0,
-            activeElement : document.querySelector('div.thumb'),
+            
 
             slides : [
                 {
@@ -69,12 +39,21 @@ const app = new Vue (
         },
 
         methods: {
-            clickToActive: function(element) {
-                if (element.classList.contains('active')){
-                    element.classList.remove('active');
-                } else {
-                    element.classList.add('active');
+            nextSlide: function() {
+                this.index = this.index + 1;
+                if (this.index === this.slides.length){
+                    this.index = 0;
                 }
+                
+                
+            },
+
+            prevSlide: function() {
+                this.index = this.index - 1;
+                if (this.index === - 1){
+                    this.index = this.slides.length - 1;
+                }
+                
                 
             }
            
